87370f2fda4d09e071dac0ca684a6658d583fe8f
dicks6n <dikkymccria@gmail.com>
2019-08-20 00:38:33 -0700

    creating app insatnce

--- manage.py
+++ manage.py
@@ -4,6 +4,13 @@ from flask_migrate import Migrate, MigrateCommand
 manager = Manager(app)
 manager.add_command('server',Server)
 
+# Creating app instance
+app = create_app("production")
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command("server", Server)
+manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
