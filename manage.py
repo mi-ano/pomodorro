162b028fd3b697c0e9a851fc0df596648a90dddf
dicks6n <dikkymccria@gmail.com>
2019-08-20 00:37:56 -0700

    importing flask script

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from app import app
-from flask_script import Manager,Server
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
 manager = Manager(app)
 manager.add_command('server',Server)
