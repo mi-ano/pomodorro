2db74985d2f5595f1241d1a5f3d09db690a70a49
dicks6n <dikkymccria@gmail.com>
2019-08-20 00:47:26 -0700

    adding user functionality

--- app/models.py
+++ app/models.py
@@ -6,3 +6,11 @@ from . import db, login_manager
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    password_hash = db.Column(db.String(255))
